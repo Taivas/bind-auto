c5e69c14c208cedf5492382a3100910b8a2966b9
CRVV <crvv.pku@gmail.com>
2015-10-14 10:41:48 +0800

    update directory structure

--- server/server.js
+++ server/server.js
@@ -5,10 +5,15 @@ let express = require('express');
 let app = express();
 let config = require('./config.js');
 
-let zone = JSON.parse(fs.readFileSync(config.zonefile));
-console.log(config.port);
 
-app.post();
+let a = JSON.parse('{"sdf": 23}');
+
+let zone = JSON.parse(fs.readFileSync(config.zonefile));
+console.log(zone);
+
+app.post('/', function (req, res) {
+    console.log(req);
+});
 
 app.listen(config.port, () => {
     console.log('Server started: http://:' + config.port + '/');
